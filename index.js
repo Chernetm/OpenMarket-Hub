ac6e7d4d99c2e0908ceff717613549862b31ff29
Chernetm <136773904+Chernetm@users.noreply.github.com>
2024-04-08 22:46:59 +0300

    I PRAISE THE GOD

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ db.once('open', () => console.log('connected to the database'));
   app.set('view engine', 'ejs');
   app.set('views', path.join(__dirname, 'views'));
   
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: true,parameterLimit:100000,limit:"50mb"}));
   app.use(methodOverride('_method'));
   app.use(express.static(path.join(__dirname, 'public')));
   
