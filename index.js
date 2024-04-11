35ba408b69dac22a086c240ad9fad8977c02c216
Chernetm <136773904+Chernetm@users.noreply.github.com>
2024-04-11 19:02:39 +0300

    praise the God

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ db.once('open', () => console.log('connected to the database'));
   app.set('view engine', 'ejs');
   app.set('views', path.join(__dirname, 'views'));
   
-  app.use(express.urlencoded({ extended: true,parameterLimit:100000,limit:"50mb"}));
+  app.use(express.urlencoded({ extended: true,limit:"50mb"}));
   app.use(methodOverride('_method'));
   app.use(express.static(path.join(__dirname, 'public')));
   
@@ -95,7 +95,13 @@ db.once('open', () => console.log('connected to the database'));
   app.use('/', routerRegister);
   app.use('/campgrounds/:id/reviews', routerViews);
   
-
+  app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 413) {
+      res.status(413).send({ error: 'Payload too large' });
+    } else {
+      next();
+    }
+  });
   
   app.all('*', (req, res, next) => {
     next(new AppError('Not page found!', 400));
